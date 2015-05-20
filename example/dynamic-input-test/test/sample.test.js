bea07573c04a98fec908075b27e0346e888dae69
Maciek Jurczyk <mjurczyk@libertyglobal.com>
2015-05-20 14:01:41 +0200

    version 0.4.2
    Feature:
    Updated dynamic-input-test to the current version.
    
    Hotfix:
    Added gl.js from example/ directory to .gitignore.

--- example/dynamic-input-test/test/sample.test.js
+++ example/dynamic-input-test/test/sample.test.js
@@ -1,21 +1,19 @@
 load('../gl.js');
 
 forAll(config.getDevices(), function (device) {
-  gl.useDevice(device);
-  
   test('Hidden popup on ' + device.deviceName, function () {
-    gl.openPage(config.getProjectPage());
+    gl.openPage(device, config.getProjectPage());
     
-    gl.runSpecFile('./test/popup-off.spec');
+    gl.runSpecFile(device, './test/popup-off.spec');
   });
   
   test('Trigger popup on ' + device.deviceName, function () {
-    gl.openPage(config.getProjectPage(), {
+    gl.openPage(device, config.getProjectPage(), {
       triggerBtn: '.trigger' 
     });
     
     elements.triggerBtn.click();
     
-    gl.runSpecFile('./test/popup-on.spec');
+    gl.runSpecFile(device, './test/popup-on.spec');
   });
 });
